ee1b740ab6af0fd3fc7f7930fa2aa67df472397d
t-addey <54467224+t-addey@users.noreply.github.com>
2019-09-19 07:42:14 +0200

    Update proservers.pp

--- myVagrantTest/environments/test/manifests/proservers.pp
+++ myVagrantTest/environments/test/manifests/proservers.pp
@@ -3,9 +3,13 @@ exec { 'apt-get update':
   command => '/usr/bin/apt-get update'
 }
 
+Exec {
+  path => ['/usr/sbin', '/usr/bin', '/sbin', '/bin', '/usr/local/bin']
+}
+
 #------Only install appserver-----#
 class appserver {
-  package { 'nodejs':
+  package { ['nodejs', 'curl', 'npm']:
      ensure => present,
      require => Exec['apt-get update']
   }  
@@ -39,18 +43,26 @@ class dbserver {
 
 #--------Using node derective------------#
 node 'web' {
-include web
+   include web
 }
 
 node 'appserver' {
-include appserver
+   include appserver
 }
 
 node 'dbserver' {
-include dbserver
+   include dbserver
 }
 
 #----Run just apt-get update on the other servers----#
 node 'tst0', 'tst1', 'tst2' {
    require => Exec['apt-get update']
+}
+
+# --- NodeJS --- #
+# Because of a package name collision, 'node' is called 'nodejs' in Ubuntu.
+# Here adding a symlink so 'node' points to 'nodejs'
+file { '/usr/bin/node':
+  ensure => 'link',
+  target => "/usr/bin/nodejs",
 }
